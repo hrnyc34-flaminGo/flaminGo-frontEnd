b57d45c00b8d66b79016e6334a423fb9e935fc92
Jimmy Ko <ko.jimmy09@gmail.com>
2021-03-03 20:21:38 -0500

    changed globalComponents references to GlobalComponents

--- client/src/components/management/ManagementLanding.jsx
+++ client/src/components/management/ManagementLanding.jsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 
 import BackArrow from '../styledElements/BackArrow';
 import FlexCenterContainer from '../styledElements/FlexCenterContainer';
-import LandingButtons from '../globalComponents/landingPage/LandingButtons';
-import useChoosePath from '../globalComponents/landingPage/useChoosePath';
+import LandingButtons from '../GlobalComponents/landingPage/LandingButtons';
+import useChoosePath from '../GlobalComponents/landingPage/useChoosePath';
 import ViewAddEditRoom from './ViewAddEditRoom.jsx';
 import ViewEmployeeList from './viewEmployeeList';
 
